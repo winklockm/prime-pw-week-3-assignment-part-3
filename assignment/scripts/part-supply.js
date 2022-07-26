0a1deb894374417dfa8fee8530024326f7fb94d4
Maggie Whitlock <maggie.whitlock@gmail.com>
2022-07-26 16:56:53 -0500

    added 25 to end of supplyChanges array

--- assignment/scripts/part-supply.js
+++ assignment/scripts/part-supply.js
@@ -23,7 +23,8 @@ console.log('4. Removed item:');
 console.log(supplyChanges.pop());
 // 5. A delivery of 25 more parts arrived. Add the value 25 to the end of the array
 console.log('5. Adding 25 to supplyChanges.');
-
+supplyChanges.push(25);
+console.log(supplyChanges);
 // 6. Write a `for` loop that shows each value in the 'supplyChanges' array
 //    Use a console.log formatted as follows, where x is the value from the array
 //    - if it is a positive number (greater than 0), log 'Added x parts.' 
