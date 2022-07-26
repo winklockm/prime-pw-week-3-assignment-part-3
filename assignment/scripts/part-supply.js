635abdd56a2c02df0665fdfb3d6dde2dcf0ef86a
Maggie Whitlock <maggie.whitlock@gmail.com>
2022-07-26 16:35:54 -0500

    console logged value of second item in supplyChanges array

--- assignment/scripts/part-supply.js
+++ assignment/scripts/part-supply.js
@@ -15,6 +15,7 @@ console.log(supplyChanges);
 
 // 3. Console log the value of the second item in the 'supplyChanges' array
 console.log('3. Second supplyChange is:');
+console.log(supplyChanges[1]);
 
 // 4. The last item was added by mistake. Remove it from the 'supplyChanges' 
 //    array & console.log the value removed.
