20fd527a74c8b18bd9eb360972fa328c139456b1
Maggie Whitlock <maggie.whitlock@gmail.com>
2022-07-26 16:32:00 -0500

    created supplyChanges variable array and console logged it

--- assignment/scripts/part-supply.js
+++ assignment/scripts/part-supply.js
@@ -10,6 +10,8 @@ console.log(partsNeeded);
 // 2. Create a variable call 'supplyChanges' set it to an array containing
 //    the following numbers: 3, 5, -6, 0, 7, 11
 console.log('2. Array of supplyChanges:');
+let supplyChanges = [3, 5, -6, 0, 7, 11];
+console.log(supplyChanges);
 
 // 3. Console log the value of the second item in the 'supplyChanges' array
 console.log('3. Second supplyChange is:');
